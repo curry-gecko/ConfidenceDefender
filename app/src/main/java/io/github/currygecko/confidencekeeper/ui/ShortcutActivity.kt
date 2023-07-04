c236ed03e4be7523875521d6582f6c8852a65162
curGeck <curry.gecko@gmail.com>
2023-07-04 23:28:28 +0900

    EditTextWithSlider の追加まで

--- app/src/main/java/io/github/currygecko/confidencekeeper/ui/ShortcutActivity.kt
+++ app/src/main/java/io/github/currygecko/confidencekeeper/ui/ShortcutActivity.kt
@@ -3,14 +3,20 @@ package io.github.currygecko.confidencekeeper.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import io.github.currygecko.confidencekeeper.model.AppInformation
+import io.github.currygecko.confidencekeeper.ui.compose.EditTextWithSlider
 import io.github.currygecko.confidencekeeper.ui.compose.list.item.AppImage
 import io.github.currygecko.confidencekeeper.ui.compose.list.item.AppInfo
 import io.github.currygecko.confidencekeeper.ui.theme.ConfidenceKeeperTheme
@@ -23,17 +29,39 @@ class ShortcutActivity : ComponentActivity() {
         val appInformation =
             receivedIntent.getParcelableExtra(AppInformation.EXTRA_KEY) as? AppInformation
 
+
         setContent {
+            // Add state here
+            var sliderValue by remember { mutableStateOf(0f) }
+            var textValue by remember { mutableStateOf("0") }
             ConfidenceKeeperTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Row(modifier = Modifier.padding(vertical = 8.dp)) {
-                        appInformation?.let {
-                            AppImage(it)
-                            AppInfo(it)
+                    Column {
+                        Row(modifier = Modifier.padding(vertical = 8.dp)) {
+                            appInformation?.let {
+                                AppImage(it)
+                                AppInfo(it)
+                            }
+                        }
+                        Row(modifier = Modifier.padding(vertical = 8.dp)) {
+                            EditTextWithSlider(
+                                sliderValue = sliderValue,
+                                onSliderValueChange = { value ->
+                                    sliderValue = value
+                                    textValue = value.toInt().toString()
+                                },
+                                textValue = textValue,
+                                onTextValueChange = { newValue ->
+                                    if (newValue.isEmpty() || newValue.toFloatOrNull() != null) {
+                                        textValue = newValue
+                                        sliderValue = newValue.toFloatOrNull() ?: 0f
+                                    }
+                                }
+                            )
                         }
                     }
                 }
